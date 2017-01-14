a6b98d2bd51af8d436552d678dceb4d9bd0981e6
Zarathon Maia <zarathonmaia@gmail.com>
2017-01-13 21:29:28 -0300

    Refactoring ResourceNotFound to Exceptions

--- src/main/java/com/greenmile/controller/TeamController.java
+++ src/main/java/com/greenmile/controller/TeamController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.BasePathAwareController;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
-import org.springframework.hateoas.EntityLinks;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.http.HttpStatus;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.greenmile.bean.Member;
 import com.greenmile.bean.Team;
